460bfd9b1b06111a3c9f1d5f5fc557ffa3ae790b
yuxuan1101 <xuan339065857@gmail.com>
2017-02-28 16:25:58 +0800

    eslint-react

--- client/src/reducers/userList.js
+++ client/src/reducers/userList.js
@@ -2,9 +2,31 @@
  * Created by yuxuan on 9/13/16.
  */
 
-export default function userList(state = [],action) {
+export default function userList (state = [], action) {
   switch (action.type) {
     default:
       return state;
   }
 }
+
+function foo() {
+  // ...
+}
+
+var bar = function () {
+  // ...
+};
+
+class Foo {
+  constructor() {
+    // ...
+  }
+}
+
+var foo = {
+  bar() {
+    // ...
+  }
+};
+
+var foo = async (a) => await a
