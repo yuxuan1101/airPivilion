5b6096889cccaff65dc963f5b609610485cdf90d
yuxuan1101 <xuan339065857@gmail.com>
2017-07-24 15:14:11 +0800

    mongo connect

--- server/app.js
+++ server/app.js
@@ -13,7 +13,9 @@ const mongoose = require('mongoose')
 const config = require('./config/config')
 
 mongoose.Promise = global.Promise
-mongoose.connect(config.database)
+mongoose.connect(config.database, {
+  useMongoClient: true
+})
 
 // middlewares
 app.use(bodyparser())
--- server/config/config.js
+++ server/config/config.js
@@ -1,25 +1,25 @@
 /**
  * Created by yuxuan on 8/11/16.
  */
-let env = process.env.NODE_ENV = process.env.NODE_ENV || "development";
+let env = process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 
 let specific = {
-    development: {
-        app: {
-            port: 3000,
-            excluded : "excluded_path"
-        },
-        database: "mongodb://localhost:27017/air-pivilion",
-        token: "screcttokenairpivilion"
+  development: {
+    app: {
+      port: 3000,
+      excluded: 'excluded_path'
     },
-    production: {
-        app: {
-            port: process.env.PORT || 3000,
-            excluded : "excluded_path"
-        },
-        database: "mongodb://localhost:27017/air-pivilion",
-        token: "screcttokenairpivilion"
+    database: 'mongodb://localhost:27017/air-pivilion',
+    token: 'screcttokenairpivilion'
+  },
+  production: {
+    app: {
+      port: process.env.PORT || 3000,
+      excluded: 'excluded_path'
     },
-};
+    database: 'mongodb://localhost:27017/air-pivilion',
+    token: 'screcttokenairpivilion'
+  }
+}
 
-module.exports = specific[env];
+module.exports = specific[env]
