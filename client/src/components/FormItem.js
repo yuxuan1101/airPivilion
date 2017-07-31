cff624d70e3f3ca116a04a9e6859b2b18a111fd5
yuxuan1101 <xuan339065857@gmail.com>
2017-07-31 18:24:17 +0800

    formItem

---
+++ client/src/components/FormItem.js
@@ -0,0 +1,58 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import FontIcon from 'material-ui/FontIcon'
+import {red100, red400, green100, green400} from 'material-ui/styles/colors'
+import pureRender from 'pure-render-decorator'
+
+@pureRender
+export default class FormItem extends React.Component {
+  static propTypes = {
+    label: PropTypes.string.isRequired,
+    disabled: PropTypes.boolean,
+    type: PropTypes.string,
+    value: PropTypes.string,
+    commit: (props, propName, componentName) => {
+      if (!(props.disabled || props.commit instanceof Function)) {
+        return new Error('Invalid prop `' + propName + '` supplied to' +
+        ' `' + componentName + '`. Validation failed.')
+      }
+    }
+  }
+  static defaultProps ={
+    disabled: false,
+    type: 'text'
+  }
+  constructor (props) {
+    super(props)
+    this.state = {
+      editing: false
+    }
+  }
+  render () {
+    let buttonStyle = {
+      cursor: 'pointer'
+    }
+    if (this.state.editing) {
+      return (
+        <div style={{display: 'flex', justifyContent: 'space-between', alignContent: 'center'}}>
+          <span style={{marginTop: '5px', width: '70px'}}>{this.props.label + ':'}</span>
+          <input type={this.props.type} defaultValue={this.props.value} ref='input'/>
+          <div>
+            <FontIcon onClick={() => this.props.commit(this.props.label, this.refs.input.value, this.props.value)} style={buttonStyle} color={green100} hoverColor={green400} className="material-icons">check</FontIcon>
+            <FontIcon onClick={() => this.setState({editing: false})} style={buttonStyle} color={red100} hoverColor={red400} className="material-icons">close</FontIcon>
+          </div>
+        </div>
+      )
+    } else {
+      return (
+        <div style={{display: 'flex', justifyContent: 'space-between', alignContent: 'center'}}>
+          <span style={{marginTop: '5px', width: '70px'}}>{this.props.label + ':'}</span>
+          <span style={{marginTop: '5px'}}>{this.props.value}</span>
+          <div>
+            {this.props.disabled ? '' : <FontIcon onClick={() => this.setState({editing: true})} style={buttonStyle} className="material-icons">edit</FontIcon>}
+          </div>
+        </div>
+      )
+    }
+  }
+}
--- client/src/components/User/index.js
+++ client/src/components/User/index.js
@@ -1,15 +1,46 @@
 import React from 'react'
+import Paper from 'material-ui/Paper'
+import Divider from 'material-ui/Divider'
+import {List, ListItem} from 'material-ui/List'
+import FormItem from '../FormItem'
 import { connect } from 'react-redux'
 import pureRender from 'pure-render-decorator'
 
 @pureRender
 class Home extends React.Component {
   render () {
-    return <div>user: {this.props.user.username}</div>
+    let commit = (key, value, oldValue) => {
+      if (value === oldValue) return
+      console.log(key, value, oldValue)
+    }
+    return (
+      <Paper style={{
+        width: '380px',
+        margin: '25px auto 0 auto',
+        padding: '5px'
+      }}>
+        <List>
+          <ListItem primaryText='头像'/>
+          <Divider/>
+          <ListItem disabled>
+            <FormItem label='名称' value={this.props.user.username} disabled/>
+          </ListItem>
+          <ListItem disabled>
+            <FormItem label='注册时间' value={this.props.user.createTime} commit={commit}/>
+          </ListItem>
+          <ListItem>
+            <FormItem label='地址' value={this.props.user.address} commit={commit}/>
+          </ListItem>
+          <ListItem>
+            <FormItem label='github' value={this.props.user.github} commit={commit}/>
+          </ListItem>
+        </List>
+      </Paper>
+    )
   }
 }
 function mapStateToProps (state) {
-  console.log(state)
+  // console.log(state)
   return {user: state.user}
 }
 export default connect(mapStateToProps)(Home)
